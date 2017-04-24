f2cdaf4834eaf281882cdb7ee1ae1c5c058c5682
Brandon Elzy <Brandon Elzy>
2017-04-23 20:38:30 -0500

    First commit

---
+++ MAC-Address-Format/MAC Address Format/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace MAC_Address_Format
+{
+    partial class frmMACAdressFormat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMACInput = new System.Windows.Forms.Label();
+            this.txtMACInput = new System.Windows.Forms.TextBox();
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.txtMACFormatted = new System.Windows.Forms.TextBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.lblFormattedMacAddresses = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMACInput
+            // 
+            this.lblMACInput.AutoSize = true;
+            this.lblMACInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMACInput.Location = new System.Drawing.Point(112, 15);
+            this.lblMACInput.Name = "lblMACInput";
+            this.lblMACInput.Size = new System.Drawing.Size(135, 17);
+            this.lblMACInput.TabIndex = 1;
+            this.lblMACInput.Text = "Enter MAC Address:";
+            // 
+            // txtMACInput
+            // 
+            this.txtMACInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMACInput.Location = new System.Drawing.Point(102, 35);
+            this.txtMACInput.MaxLength = 17;
+            this.txtMACInput.Name = "txtMACInput";
+            this.txtMACInput.Size = new System.Drawing.Size(154, 23);
+            this.txtMACInput.TabIndex = 0;
+            this.txtMACInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFormat.Location = new System.Drawing.Point(131, 79);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(97, 27);
+            this.btnFormat.TabIndex = 1;
+            this.btnFormat.Text = "Format";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            // 
+            // txtMACFormatted
+            // 
+            this.txtMACFormatted.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMACFormatted.Location = new System.Drawing.Point(12, 255);
+            this.txtMACFormatted.Multiline = true;
+            this.txtMACFormatted.Name = "txtMACFormatted";
+            this.txtMACFormatted.ReadOnly = true;
+            this.txtMACFormatted.Size = new System.Drawing.Size(334, 159);
+            this.txtMACFormatted.TabIndex = 2;
+            this.txtMACFormatted.Text = "\r\n";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(127, 124);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 3;
+            // 
+            // lblFormattedMacAddresses
+            // 
+            this.lblFormattedMacAddresses.AutoSize = true;
+            this.lblFormattedMacAddresses.Location = new System.Drawing.Point(13, 236);
+            this.lblFormattedMacAddresses.Name = "lblFormattedMacAddresses";
+            this.lblFormattedMacAddresses.Size = new System.Drawing.Size(135, 13);
+            this.lblFormattedMacAddresses.TabIndex = 4;
+            this.lblFormattedMacAddresses.Text = "Formatted MAC Addresses:";
+            // 
+            // frmMACAdressFormat
+            // 
+            this.AcceptButton = this.btnFormat;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(358, 426);
+            this.Controls.Add(this.lblFormattedMacAddresses);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.txtMACFormatted);
+            this.Controls.Add(this.btnFormat);
+            this.Controls.Add(this.txtMACInput);
+            this.Controls.Add(this.lblMACInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMACAdressFormat";
+            this.Text = "MAC Address Format v1.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblMACInput;
+        private System.Windows.Forms.TextBox txtMACInput;
+        private System.Windows.Forms.Button btnFormat;
+        private System.Windows.Forms.TextBox txtMACFormatted;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label lblFormattedMacAddresses;
+    }
+}
+
---
+++ MAC-Address-Format/MAC Address Format/Form1.cs
@@ -0,0 +1,489 @@
+﻿// Application Name: MAC Address Format Tool
+// Author: Brandon G. Elzy
+// Date Created: April 11, 2017
+// Date Last Modified: April 23, 2017
+// Purpose: This application allows a user to enter a 48 bit MAC address string in a number of different formats.
+//          When the format button is activated, the string is converted into all available MAC Address formats and
+//          displayed in a text box. The user may copy any of the text from the textbox, but cannot edit the text.
+
+// UPDATES:
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ *  - Version 1.1
+ *  - April 23, 2017
+ *  - Added feature where user can enter any of the MAC Address formats as input.
+ *  - Optimized runtime by refactoring the input validation and formatting processes.
+ *    
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 
+ */
+
+// BUGS:
+/*  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * - After entering the format (123456-789abc) then adding a character (123456789-abcd)
+ * - the application does not crash but seems to be loading and always displays the invalidMacAddress
+ * - error message.
+ * 
+ *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ *  - NOTE: Some documentation is incorrect due to refactoring.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MAC_Address_Format
+{
+    public partial class frmMACAdressFormat : Form
+    {
+        // Declare variables.
+        private static string emptyString = "";                             // An empty string for clearing form control text.
+        private static string invalidMacAddress = "Invalid MAC Address";    // Invalid MAC address error message.
+
+        // Declare functions.
+        private static void DisplayFormattedMacAddresses(string[] macs, TextBox txt)
+        {
+            foreach(string m in macs)
+            {
+                 txt.Text += m + Environment.NewLine;
+            }
+        }   // Adds each element of an array to a text box with each on a new line.
+
+        public frmMACAdressFormat()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            // Clear the form controls.
+            txtMACFormatted.Text = emptyString;
+            lblError.Text = emptyString;
+            MAC.Clear();
+
+            if (MAC.FormatAddress(txtMACInput.Text))
+            {
+                string[] macAddresses = MAC.getFormattedMacAddresses();
+                DisplayFormattedMacAddresses(macAddresses, txtMACFormatted);
+            }
+            else
+            {
+                lblError.Text = invalidMacAddress;
+            }
+        }
+    }
+
+    public class MAC
+    {
+        // ACCEPTED MAC ADDRESS FORMATS:
+        /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+         * 1) 123456789abc
+         * 2) 123456-789abc
+         * 3) 1234.5678.9abc
+         * 4) 12.34.56.78.9a.bc
+         * 5) 12-34-56-78-9a-bc
+         * 6) 12:34:56:78:9a:bc
+         *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+         */
+
+        // Declare variables.
+        private static string[] formattedMacAddresses = new string[6];           // This array will contain the formatted MAC Addresses.
+        private static char[] formatCharacters = new char[] { '.', '-', ':' };   // This array contains the acceptable format characters to be used in the validation and fomatting processes.
+        private static int[] formatLengths = new int[] {12, 13, 14, 17 };        // This array contains all acceptable lengths of a mac address input string.
+        private static int position = 0;                                         // Represents the position of the character in the string being tested against the format character.
+
+        // Public class functions.
+        public static bool FormatAddress(string mac)
+        {   // This function takes a 48 bit MAC Address string as an argument.
+            // It will check if the string matches one of the acceptable formats.
+            // If the string matches one of the formats, it will convert it to the other formats and return true.
+            // If the string doesn't match one of the formats, the function will return FALSE.
+
+            // If argument 'string mac' format is correct, convert it, format it, and assign it to the formattedMacAddresses array.
+            if (ValidateMacAddress(mac))
+            {
+                string convertedMacAddress = ConvertMacAddress(mac);        // Converted MAC address to be used in Format functions.
+                formattedMacAddresses[0] = FormatOne(convertedMacAddress);
+                formattedMacAddresses[1] = FormatTwo(convertedMacAddress);
+                formattedMacAddresses[2] = FormatThree(convertedMacAddress);
+                formattedMacAddresses[3] = FormatFour(convertedMacAddress);
+                formattedMacAddresses[4] = FormatFive(convertedMacAddress);
+                formattedMacAddresses[5] = FormatSix(convertedMacAddress);
+
+                return true;
+            }
+            // If MAC Address input format is not correct, return false.
+            return false;
+        }
+        public static string[] getFormattedMacAddresses()
+        {
+            return formattedMacAddresses;
+        }     // Returns the formattedMacAddresses array.
+        public static void Clear()
+        {   // This function will clear the values from the formattedMacAddresses array.
+            for (int i = 0; i < formattedMacAddresses.Length; i++)
+            {
+                formattedMacAddresses[i] = "";
+            }
+        }
+
+        // Private class functions.
+        private static bool ValidateMacAddress(string mac)
+        {   // This function takes a 48 bit MAC Address string as an argument.
+            // It will check if the string matches one of the acceptable formats.
+            // If the string matches one of the formats, the function will return TRUE.
+            // If the string doesn't match one of the formats, the function will return FALSE.
+
+            // Check first format.
+            if (mac.Length == formatLengths[0])
+            {
+                foreach (char c in mac)
+                {
+                    foreach (char f in formatCharacters)
+                    {
+                        if (c == f)
+                            return false;
+                    }
+                }
+                return true;
+            }
+
+            // Check second format.
+            else if (mac.Length == formatLengths[1])
+            {
+                foreach (char c in mac)
+                {
+                    if ((++position % 7) == 0)
+                    {
+                        if (c == formatCharacters[1])
+                        {
+                            if (position == 7)
+                            {
+                                position = 0;
+                                return true;
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+
+            // Check third format.
+            else if (mac.Length == formatLengths[2])
+            {
+                foreach (char c in mac)
+                {
+                    if ((++position % 5) == 0)
+                    {
+                        if (c == formatCharacters[0])
+                        {
+                            if (position == 10)
+                            {
+                                position = 0;
+                                return true;
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+            else if (mac.Length == formatLengths[3])
+            {
+                // Check fourth format.
+                foreach (char c in mac)
+                {
+                    if ((++position % 3) == 0)
+                    {
+                        if (c == formatCharacters[0])
+                        {
+                            if (position == 15)
+                            {
+                                position = 0;
+                                return true;
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+
+                // Check fifth format.
+                foreach (char c in mac)
+                {
+                    if ((++position % 3) == 0)
+                    {
+                        if (c == formatCharacters[1])
+                        {
+                            if (position == 15)
+                            {
+                                position = 0;
+                                return true;
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+
+                // Check sixth format.
+                foreach (char c in mac)
+                {
+                    if ((++position % 3) == 0)
+                    {
+                        if (c == formatCharacters[2])
+                        {
+                            if (position == 15)
+                            {
+                                position = 0;
+                                return true;
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+
+            // If mac string doesn't match any of the acceptable formats, return false.
+            return false;
+        }    // Validates a MAC address string.
+        private static string ConvertMacAddress(string mac)
+        {   // This function will convert the MAC Address string from its original format into
+            // a format that can be used in the format functions. This function should only be called if
+            // the ValidateMacAddress function is called first and returns true.
+
+            // Declare variables.
+            string convertedMacAddress = "";    // The converted MAC Address string to be returned.
+
+            // Convert first format
+            if (mac.Length == formatLengths[0])
+            {
+                convertedMacAddress = mac;
+                return convertedMacAddress;
+            }
+
+            // check second format
+            else if (mac.Length == formatLengths[1])
+            {
+                foreach (char c in mac)
+                {
+                    if ((++position % 7) == 0)
+                    {
+                        if (c == formatCharacters[1])
+                        {
+                            if (position == 7)
+                            {
+                                position = 0;
+
+                                // Convert format.
+                                foreach (char ch in mac)
+                                {
+                                    if (ch != formatCharacters[1])
+                                        convertedMacAddress += ch;
+                                }
+                                return convertedMacAddress;
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+
+            // check third format
+            else if (mac.Length == formatLengths[2])
+            {
+                foreach (char c in mac)
+                {
+                    if ((++position % 5) == 0)
+                    {
+                        if (c == formatCharacters[0])
+                        {
+                            if (position == 10)
+                            {
+                                position = 0;
+
+                                // Convert format.            
+                                foreach (char ch in mac)
+                                {
+                                    if (ch != formatCharacters[0])
+                                        convertedMacAddress += ch;
+                                }
+                                return convertedMacAddress;
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+            else if (mac.Length == formatLengths[3])
+            {
+                // check fourth format
+                foreach (char c in mac)
+                {
+                    if ((++position % 3) == 0)
+                    {
+                        if (c == formatCharacters[0])
+                        {
+                            if (position == 15)
+                            {
+                                position = 0;
+
+                                // Convert format.            
+                                foreach (char ch in mac)
+                                {
+                                    if (ch != formatCharacters[0])
+                                        convertedMacAddress += ch;
+                                }
+                                return convertedMacAddress;
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+
+                // Check fifth format
+                foreach (char c in mac)
+                {
+                    if ((++position % 3) == 0)
+                    {
+                        if (c == formatCharacters[1])
+                        {
+                            if (position == 15)
+                            {
+                                position = 0;
+
+                                // Convert format.            
+                                foreach (char ch in mac)
+                                {
+                                    if (ch != formatCharacters[1])
+                                        convertedMacAddress += ch;
+                                }
+                                return convertedMacAddress;
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+
+                // Check sixth format
+                foreach (char c in mac)
+                {
+                    if ((++position % 3) == 0)
+                    {
+                        if (c == formatCharacters[2])
+                        {
+                            if (position == 15)
+                            {
+                                position = 0;
+
+                                // Convert format.            
+                                foreach (char ch in mac)
+                                {
+                                    if (ch != formatCharacters[2])
+                                        convertedMacAddress += ch;
+                                }
+                                return convertedMacAddress;
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+            }
+            return convertedMacAddress;
+        }   // Returns convertedMacAddress.
+        private static string FormatOne(string mac)
+        {
+            return mac;
+        }           // Formats the convertedMacAddress to format #1.
+        private static string FormatTwo(string mac)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var c in mac)
+            {
+                builder.Append(c);
+                if ((++position % 6) == 0)
+                {
+                    if (position != 12)
+                        builder.Append('-');
+                }
+            }
+            position = 0;
+            return builder.ToString();
+        }           // Formats the convertedMacAddress to format #2.
+        private static string FormatThree(string mac)
+        {   // This function takes a 48 bit MAC Address string as an argument.
+            // It will add a '.' after every fourth character in the string then
+            // return the formatted string.
+
+            StringBuilder builder = new StringBuilder();
+            foreach (var c in mac)
+            {
+                builder.Append(c);
+                if ((++position % 4) == 0)
+                {
+                    if (position != 12)
+                        builder.Append('.');
+                }
+            }
+            position = 0;
+            return builder.ToString();
+        }         // Formats the convertedMacAddress to format #3.
+        private static string FormatFour(string mac)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (char c in mac)
+            {
+                builder.Append(c);
+                if ((++position % 2) == 0)
+                {
+                    if (position != 12)
+                        builder.Append(formatCharacters[0]);
+                }
+            }
+            position = 0;
+            return builder.ToString();
+        }          // Formats the convertedMacAddress to format #4.
+        private static string FormatFive(string mac)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (char c in mac)
+            {
+                builder.Append(c);
+                if ((++position % 2) == 0)
+                {
+                    if (position != 12)
+                        builder.Append(formatCharacters[1]);
+                }
+            }
+            position = 0;
+            return builder.ToString();
+        }          // Formats the convertedMacAddress to format #5.
+        private static string FormatSix(string mac)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (char c in mac)
+            {
+                builder.Append(c);
+                if ((++position % 2) == 0)
+                {
+                    if (position != 12)
+                        builder.Append(formatCharacters[2]);
+                }
+            }
+            position = 0;
+            return builder.ToString();
+        }           // Formats the convertedMacAddress to format #6.
+    }
+}
